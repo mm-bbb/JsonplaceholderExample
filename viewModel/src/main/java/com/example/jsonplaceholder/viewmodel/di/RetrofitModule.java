0aeafe5621aa402608765c819deb15e711d94628
mwbaek <mwbaek6457@gmail.com>
2022-10-08 18:31:38 +0900

    Fix RetrofitModule

--- viewModel/src/main/java/com/example/jsonplaceholder/viewmodel/di/RetrofitModule.java
+++ viewModel/src/main/java/com/example/jsonplaceholder/viewmodel/di/RetrofitModule.java
@@ -4,26 +4,50 @@ import com.example.jsonplaceholder.model.data.CommentService;
 import com.example.jsonplaceholder.model.data.PostService;
 import com.example.jsonplaceholder.model.data.UserService;
 
+import java.util.concurrent.TimeUnit;
+
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
 import dagger.Reusable;
 import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 @Module
 public class RetrofitModule {
+
     @Provides
     @Singleton
-    Retrofit provideRetrofit() {
+    HttpLoggingInterceptor provideLoggingInterceptor() {
+        return new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY);
+    }
+
+    @Provides
+    @Singleton
+    OkHttpClient provideOkHttpClient(HttpLoggingInterceptor interceptor) {
+        return new OkHttpClient.Builder()
+                .connectTimeout(20, TimeUnit.SECONDS)
+                .writeTimeout(20, TimeUnit.SECONDS)
+                .readTimeout(20, TimeUnit.SECONDS)
+                .addInterceptor(interceptor)
+                .build();
+    }
+
+    @Provides
+    @Singleton
+    Retrofit provideRetrofit(OkHttpClient okHttpClient) {
         return new Retrofit.Builder()
                 .baseUrl("https://jsonplaceholder.typicode.com/")
+                .client(okHttpClient)
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
                 .build();
     }
+
     @Provides
     @Reusable
     PostService providePostService(Retrofit retrofit) {
